9dce445c4584325ba91618c6292785529028a21f
Antonina Kobzar <antonina.kobzar@gmail.com>
2024-03-10 20:33:39 +0200

    init

---
+++ src/components/Part.jsx
@@ -0,0 +1,14 @@
+const Part = (props) => {
+    
+  
+    return (
+        <div>
+        <p>{props.part} {props.exercises}</p>
+        
+       
+      </div>
+      
+    )
+  }
+  
+  export default Part

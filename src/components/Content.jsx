f8f860016252c7b628e1ed0c7fc944c4c3e5feaf
Antonina Kobzar <antonina.kobzar@gmail.com>
2024-03-10 21:00:11 +0200

    3

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+import Part from "./Part"
+
+const Content = () => {
+    
+      return (
+        <div>
+          <Part part={'Fundamentals of React'} exercises={10}/>
+          <Part part={'Using props to pass data'} exercises={7}/>
+          <Part part={'State of a component'} exercises={14}/>
+        </div>
+      )
+      
+    
+  }
+  
+  export default Content
---
+++ src/components/Total.jsx
@@ -0,0 +1,11 @@
+const Total = (props) => {
+    
+ 
+    return (
+        <div>
+        <p>Number of exercises {props.exercises}</p>
+      </div>
+    )
+  }
+  
+  export default Total
